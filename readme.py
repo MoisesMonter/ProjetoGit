984bd25474e6a6af758ba66881586f9d01e1a6ff
Moises Monteiro <moisesmonteiro2016@hotmail.com>
2022-02-05 19:15:42 -0300

    atualização

--- readme.py
+++ readme.py
@@ -1,1 +1,10 @@
-print("primeiro git")
+class world:
+    def __init__(self,bol):
+        self.bol = bol
+    def verdadeiro(self):
+        if (self.bol == True):
+            print("hello world")
+        elif(self.bol == False):
+            print("END world")
+    def novalinha(self):
+        print("use sempre um git diff antes de dar um commit")
